861d4c64cf015122102c8845b2e09d362adf6cf7
IsaiasTolP <toledop.isaias@gmail.com>
2024-10-29 16:38:21 +0000

    Se añade estilo login

--- tribu/shared/forms.py
+++ tribu/shared/forms.py
@@ -7,7 +7,7 @@ class SignupForm(forms.ModelForm):
         model = get_user_model()
         fields = ('first_name', 'last_name', 'username', 'email', 'password')
         widgets = dict(password=forms.PasswordInput)
-    
+
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         for visible in self.visible_fields():
